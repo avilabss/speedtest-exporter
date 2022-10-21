65da036850054b9442869531287c0699f3315259
git-avinash <mail.avinashsah@gmail.com>
2022-10-21 22:22:31 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.10.4-alpine3.15
+
+ARG SPEEDTEST_VERSION=1.2.0
+RUN adduser -D speedtest
+
+WORKDIR /app
+COPY . .
+
+RUN ARCHITECTURE=$(uname -m) && \
+    export ARCHITECTURE && \
+    if [ "$ARCHITECTURE" = 'armv7l' ];then ARCHITECTURE="armhf";fi && \
+    wget -nv -O /tmp/speedtest.tgz "https://install.speedtest.net/app/cli/ookla-speedtest-${SPEEDTEST_VERSION}-linux-${ARCHITECTURE}.tgz" && \
+    tar zxvf /tmp/speedtest.tgz -C /tmp && \
+    cp /tmp/speedtest /usr/local/bin && \
+    chown -R speedtest:speedtest /app && \
+    rm -rf \
+     /tmp/* \
+     /app/requirements
+
+RUN pip3 install -r requirements.txt
+USER speedtest
+# CMD [ "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000" ]
+CMD [ "python3", "main.py" ]
---
+++ main.py
@@ -0,0 +1,149 @@
+import os
+import json
+import time
+import logging
+import subprocess
+
+from flask import Flask
+from prometheus_client import make_wsgi_app, Gauge
+from waitress import serve
+from shutil import which
+
+
+# Create Flask app
+app = Flask("speedtest-exporter")
+
+# Setup logging
+logging.basicConfig(
+    encoding="utf-8",
+    format="%(levelname)s:     %(message)s",
+)
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+# Create Metrics
+server = Gauge("speedtest_server_id", "Speedtest server ID used to test")
+jitter = Gauge("speedtest_jitter_latency_milliseconds", "Speedtest current Jitter in ms")
+ping = Gauge("speedtest_ping_latency_milliseconds", "Speedtest current Ping in ms")
+download_speed = Gauge("speedtest_download_bits_per_second", "Speedtest current Download Speed in bit/s")
+upload_speed = Gauge("speedtest_upload_bits_per_second", "Speedtest current Upload speed in bits/s")
+status = Gauge("speedtest_status", "Speedtest status for whether the scrape worked")
+
+# Cache speedtest results
+speedtest_cache_timeout = time.time()
+
+
+def bytes_to_bits(bytes_per_sec):
+    return bytes_per_sec * 8
+
+
+def bits_to_megabits(bits_per_sec):
+    megabits = round(bits_per_sec * (10**-6), 2)
+    return megabits
+
+
+def is_json(json_data) -> bool:
+    try:
+        json.loads(json_data)
+
+    except ValueError:
+        return False
+
+    return True
+
+
+def run_speedtest(server_id: str = None, timeout: int = None) -> tuple:
+    cmd = [
+        "speedtest", 
+        "--format=json-pretty", 
+        "--progress=no",
+        "--accept-license", 
+        "--accept-gdpr",
+    ]
+
+    if server_id:
+        cmd.append(f"--server-id={server_id}")
+
+    try:
+        output = subprocess.check_output(cmd, timeout=timeout)
+
+    except subprocess.CalledProcessError as e:
+        output = e.output
+        if not is_json(output):
+            log.error("Speedtest CLI error not in JSON format.")
+            return (0, 0, 0, 0, 0, 0)
+
+    if is_json(output):
+        data = json.loads(output)
+
+        if "error" in data:
+            log.error("Socker error while speedtest.")
+            log.error(data["error"])
+            return (0, 0, 0, 0, 0, 0)
+
+        if "type" in data:
+            if data["type"] == "log":
+                log.info(str(data["timestamp"]) + " - " + str(data["message"]))
+
+            if data["type"] == "result":
+                actual_server = int(data["server"]["id"])
+                actual_jitter = data["ping"]["jitter"]
+                actual_ping = data["ping"]["latency"]
+                actual_download_speed = bytes_to_bits(data["download"]["bandwidth"])
+                actual_upload_speed = bytes_to_bits(data["upload"]["bandwidth"])
+                return (actual_server, actual_jitter, actual_ping, actual_download_speed, actual_upload_speed, 1)
+
+    log.error("Successfull speedtest had no json result.")
+    return (0, 0, 0, 0, 0, 0)
+
+
+@app.route("/metrics")
+def record_speedtest():
+    global speedtest_cache_timeout
+    
+    cache_timeout = int(os.getenv("SPEEDTEST_CACHE_TIMEOUT", 900))
+    speedtest_server = os.getenv("SPEEDTEST_SERVER", None)
+    speedtest_run_timeout = int(os.getenv("SPEEDTEST_RUN_TIMEOUT", 90))
+
+    log.info("Generating and Recording metrics for speedtest.")
+
+    if time.time() > speedtest_cache_timeout:
+        log.info("Cache timeout, Processing new results.")
+
+        r_server, r_jitter, r_ping, r_download, r_upload, r_status = run_speedtest(speedtest_server, speedtest_run_timeout)
+
+        log.info(f"Server - {str(r_server)}") 
+        server.set(r_server)
+        
+        log.info(f"Jitter - {str(r_jitter)}") 
+        jitter.set(r_jitter)
+        
+        log.info(f"Ping - {str(r_ping)}") 
+        ping.set(r_ping)
+        
+        log.info(f"Download - {str(r_download)}") 
+        download_speed.set(r_download)
+        
+        log.info(f"Upload - {str(r_upload)}") 
+        upload_speed.set(r_upload)
+        
+        log.info(f"Status - {str(r_status)}")
+        status.set(r_status)
+
+        speedtest_cache_timeout = time.time() + cache_timeout
+
+    return make_wsgi_app()
+
+
+@app.route("/")
+def index():
+    return f"Speedtest exporter 🐲"
+
+
+if __name__ == "__main__":
+    HOST = os.getenv("SPEEDTEST_HOST", "0.0.0.0")
+    PORT = os.getenv("SPEEDTEST_PORT", 8000)
+    
+    log.info(f"Starting exporter on http://{HOST}:{PORT}")
+    serve(app, host="0.0.0.0", port=PORT)
